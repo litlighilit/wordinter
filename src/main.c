c0d6c32a7aa66cb590f40b61d5300ad1cb14e6ff
litlighilit <litlighilit@foxmail.com>
2023-12-12 01:08:08 +0800

    feature-add: support multi `-c` passed; support `-f` flag to add files to read; `support long option e.g. `--code` is supported now

--- src/main.c
+++ src/main.c
@@ -6,43 +6,90 @@
 #include "seq.h"
 #include "fsutil.h" // listDir
 #include "interpreter.h" // enterRepl
-#include "cli.h"
+#include "aparsearg.h"
 #include "msg.h" // err
 
 #include "main.h" // HelpMsg, Version
 
 int main(int argc, char* argv[]){
-    if(argc==1) err("please pass a directory path as a argument");
+    
+    ArgParser parser = newArgParser(argc, argv, detailsHelp);
 
-    int passed = 1; // pass argv[0]
-    const char** restArgs = (const char**)(argv+passed);
-    Args args = parseArg(restArgs, argc-passed);
+    INFO.usages = genUsage(); 
+    enInfo(parser, INFO);
 
-    if(args.help) goto PHelp;
-    else if(args.version) goto PVersion;
+    addBoolOpt(parser, singLineP, lHelp);
+    addArgHelp(parser, "dir", "dir to process");
 
-    RecSeq rs = listDir(args.arg);
+    addListKey(parser, codesK, "CODE", cHelp);
+    addArgMap(parser, codesK, "--code");
+
+    addListKey(parser, filesK, "FILE", fHelp);
+    addArgMap(parser, filesK, "--file");
+
+    ParseArgRes args = parseArgs(parser);
+    if(args.err!=NULL) err("error: %s\n", args.err);
+    if(args.args.len>1)
+        err("only one positional argument as a directory to read from is allowed,\n"
+            " but got %d\n"
+            " however, you can pass any number of files as input via `-f`", args.args.len);
+
+    bool hasInput=false;
+
+    RecSeq rs;
+    if(args.args.len!=0){
+        CharSeq dirS=getItem(args.args, 0);
+        char* dir = cstr(dirS);
+        rs = listDir(dir);
+        hasInput=true;
+        free(dir);
+    }else{
+        initSeq(Rec, rs);
+    }
+
+    ListArg files = getListVal(args, filesK);
+    forIndex(i, files){
+        hasInput=true;
+        CharSeq filepath=getItem(files, i);
+        char*cfilepath=cstr(filepath);
+        FILE* fp = fopen(cfilepath, "r");
+        if(fp==NULL){
+            warn("can't read '%s' ...", cfilepath);
+        }
+        CharSeq content = readAll(fp);
+        Rec rec={.fname=cfilepath, .data=content};
+        addItem(rs, rec);
+        // do not free(cfilepath) and deinitSeq(char, content), as they're owned by rec now.
+
+    }
+    if(!hasInput) err("neither a dir nor some files is passed. please pass some input data");
+
+
+    bool b=wantOpt(args, singLineP); // here, cause as we consider -L as default, this wantOpt can make what we want
+                                     //  otherwise(if we want -l as default), it can't be equalified
+    bool multiLinePara = !b;
+
     if(rs.len==0) {
         err("can't read any data from dir");
     }
 
-    Interpreter interp = initInterpreter(rs, args.multiLinePara);
-    if( args.cmd.len!=0 ){
-        evalCmd(&interp, args.cmd);
-        deinitSeq(args.cmd);
+    Interpreter interp = initInterpreter(rs, multiLinePara);
+
+
+    ListArg cmds = getListVal(args, codesK);
+
+    if( cmds.len!=0 ){
+        forIndex(i, cmds){
+            CharSeq cmd = getItem(cmds, i);
+            evalCmd(&interp, cmd);
+        }
     }else{
         enterRepl(&interp);
     }
+    deInfo(INFO);
+    freeParseArgRes(args);
+    freeArgParser(parser);
     return 0;
 
-PHelp:
-    PriInfo(ProjName);
-    PriInfo(Version);
-    PriInfo(License);
-    msgl(HelpMsg);
-    return 0;
-
-PVersion:
-    msgl(Version);
 }
 
--- src/main.h
+++ src/main.h
@@ -13,8 +13,42 @@ usage: \n\
       -L\tmultiline mode, only if there are more than 1 newline,\n\
         \tthen they are treated as two paragraphs  [default mode]\
 "
-#define License "Copyright (c) 2023 litlighilit(lcm). GPLv3 License"
+
+#define Author "litlighilit(lcm)"
+#define License "GPLv3"
 #define Version "1.0.0"
 
-#define PriInfo(item) msgl( #item ": %s", item)
+#define Proj "WordInter"
+#define Desc "a file-based search engine"
+
+#define K(arr) arr[1]
+
+#define singleLineParaFlag "-l"
+const char singLineP = K(singleLineParaFlag);
+#define lHelp "single line mode, treat each line as a paragragh."
+#define detailsHelp "if '-l' is not set, it'll be multiline mode[default mode], only if there are more than 1 newline,\n\
+        \tthen they are treated as two paragraphs"
+
+#define filesKey "-f"
+const char filesK = K(filesKey);
+#define fHelp "files to read from"
+
+#define codesKey "-c"
+const char codesK = K(codesKey);
+#define cHelp "code to run, try -c 'help CODE' for specific CODE's help"
+
+ProjInfo INFO = (ProjInfo){
+        .auther=Author, 
+        .license=License,
+
+        .proj=Proj,
+        .version=Version,
+        .desc=Desc,
+        //.usages=... // will (and have to be) set within `main` function
+    };
+Usages genUsage(){ return USAGES(
+             "[-l|-L] [-c,--code CODE] dir|[-f,--file FILE]...\t run code and exit"
+            ,"[-l|-L] dir|[-f,--file FILE]...\t\t\t enter REPL"
+        );
+}
 #endif
