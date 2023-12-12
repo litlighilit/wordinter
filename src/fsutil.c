a0cd297e8bf7982ea4dd078a60d54b97b8892b41
litlighilit <litlighilit@foxmail.com>
2023-12-13 00:13:34 +0800

    feature-add: may support MSVC compiler now (in mod fsutils, using condational compile to use <io.h> instead of <dirent.h> if MSVC)

--- src/fsutil.c
+++ src/fsutil.c
@@ -3,7 +3,6 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <dirent.h>
 
 #include "fsutil.h"
 
@@ -21,6 +20,62 @@ CharSeq readAll(FILE* f){
 }
 
 
+// can't easly deined as a func
+// a tricky macro with a `continue` stmt
+#define PushResWithName(res, fname) do{\
+        char* fpath = joinPath(dir, fname);\
+        FILE* f=fopen(fpath, "r");\
+        free(fpath);\
+        if(f==NULL){\
+            continue;\
+        }\
+        CharSeq content = readAll(f);\
+        fclose(f);\
+        \
+        Rec rec;\
+        rec.fname = newCStr(fname);\
+        rec.data = content;\
+        \
+        addItem(res, rec);\
+}while(0);
+
+
+#ifdef _MSC_VER
+#include <io.h>
+
+RecSeq listDir(const char* dir){
+    RecSeq res;
+    initSeq(Rec, res);
+
+    const char* pattern = joinPath(dir, "*");
+    struct _finddata_t fileInfo;
+    intptr_t handle = _findfirst(pattern, &fileInfo);
+
+    if (handle == -1L) {
+        return res;
+    }
+
+    int k;
+    do {
+        char*fname = fileInfo.name;
+        if(strcmp(fname, ".")==0 || strcmp(fname, "..")==0){
+            goto Next;
+        }
+        PushResWithName(res, fname)
+        
+        Next:
+        k = _findnext(handle, &fileInfo);
+    } while (k==0);
+     
+    _findclose(handle);
+    free((char*)pattern);
+    return res;
+}
+
+//#elif defined(_WIN32) && !defined(__MINGW64_VERSION_MAJOR)
+#else
+
+#include <dirent.h>
 RecSeq listDir(const char* dir){
     RecSeq res;
     initSeq(Rec, res);
@@ -36,25 +91,13 @@ RecSeq listDir(const char* dir){
 
         if(strcmp(fname, ".")==0 || strcmp(fname, "..")==0) continue;
 
-        char* fpath = joinPath(dir, fname);
-        FILE* f=fopen(fpath, "r");
-        free(fpath);
-        if(f==NULL){
-            //fprintf(stderr, "can't open %s as file, skip it\n");
-            continue;
-        }
-        
-        CharSeq content = readAll(f);
-        fclose(f);
+        PushResWithName(res, fname)
 
-        Rec rec;
-        rec.fname = newCStr(fname);
-        rec.data = content;
-
-        addItem(res, rec);
     }
     closedir(dp);
     
     return res;
 }
 
+
+#endif
