dec4a0d39701b37a7ee372a21bea6905bde08ce6
litlighilit <litlighilit@foxmail.com>
2024-01-04 16:35:46 +0800

    bug-fix: totally support check range of seq

--- src/parseutils.c
+++ src/parseutils.c
@@ -11,13 +11,13 @@
     return i;\
 }while(0)
 
-#define RETparseUntilBy(s, check, start) do{\
+#define RETparseUntilBy(s, checkCharSeqAt, start) do{\
     CharSeq res;\
     initSeq(char, res);\
     int i=0;\
     for(; ; i++){\
         int idx = i+start;\
-        if( idx>=s.len || check(getItem(s, idx)) ) break;\
+        if( idx>=s.len || checkCharSeqAt(s, idx) ) break;\
         addItem(res, getItem(s,idx));\
     }\
     return res;\
@@ -31,13 +31,13 @@ int skip(const CharSeq s, char skipped, int start){
 }
 
 CharSeq parseUntil(const CharSeq s, char until, int start){
-  #define charEq(c) (c==until)
+  #define charEq(s, idx) (getItem(s,idx)==until)
     RETparseUntilBy(s, charEq, start);
   #undef charEq
 }
 
 CharSeq parseUntil2(const CharSeq s, char until, int start){
-  #define charEq(c) (c==until&& *(&(c)+1)==until )
+  #define charEq(s, idx) (getItem(s, idx)==until && idx+1 < s.len && getItem(s,idx+1)==until)
     RETparseUntilBy(s, charEq, start);
   #undef charEq
 }
@@ -48,7 +48,8 @@ int skipPunc(const CharSeq s, int start){
     RETskipBy(s, isPunc, start);
 }
 
+#define isPuncAt(s, idx) isPunc(getItem(s,idx))
 CharSeq parseUntilPunc(const CharSeq s, int start){
-    RETparseUntilBy(s, isPunc, start);
+    RETparseUntilBy(s, isPuncAt, start);
 }
 
