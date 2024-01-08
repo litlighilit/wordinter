1b56cdb0c3a4db124184a00ddafa042f12cab0f2
litlighilit <litlighilit@foxmail.com>
2024-01-09 00:27:49 +0800

    bump-version: 1.1.1

--- src/main.h
+++ src/main.h
@@ -11,7 +11,7 @@
 
 #define Author "litlighilit(lcm)"
 #define License "GPLv3"
-#define Version "1.1.0"
+#define Version "1.1.1"
 
 #define Proj "WordInter"
 #define Desc "a file-based search engine"
